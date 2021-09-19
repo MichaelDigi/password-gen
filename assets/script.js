6295e0acf9d2bdd6fe494a13a5a956835c0746b6
Demon <zero2211@protonmail.com>
2021-09-19 16:31:49 -0400

    fixed problem with js file

--- assets/script.js
+++ assets/script.js
@@ -6,10 +6,64 @@ let confirmLower;
 let confirmSpecial;
 let confirmNumbers;
 
-let charsUpper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
-let charsLower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-let charsSpecial = ['!','@','#','$','%','^','&','*', '(',')'];
-let charsNumbers = ['0','1','2','3','4','5','6','7','8','9'];
+let charsUpper = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+let charsLower = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let charsSpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+let charsNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 let characters = [];
 
@@ -26,15 +80,13 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
- passwordLength = prompt("Choose password length 8-128");
+  passwordLength = prompt("Choose password length 8-128");
   console.log(passwordLength);
   if (!passwordLength) {
     alert("No value entered");
-  }
-  else if(passwordLength < 8 || passwordLength > 128) {
+  } else if (passwordLength < 8 || passwordLength > 128) {
     passwordLength = prompt("Must enter a value between set range 8-128");
-  }
-  else {
+  } else {
     confirmUpper = confirm("Do you want to use uppercase characters?");
     confirmLower = confirm("Do you want to use lowercase characters?");
     confirmSpecial = confirm("Do you want to use special characters?");
@@ -42,68 +94,77 @@ function generatePassword() {
   }
 
   if (!(confirmUpper && confirmLower && confirmSpecial && confirmNumbers)) {
-    alert("please select one of the options")
+    alert("please select one of the options");
   }
 
-  if (confirmLower){
+  if (confirmLower) {
     characters = charsLower;
     console.log(characters);
   }
 
-  if (confirmUpper){
+  if (confirmUpper) {
     characters = charsUpper;
   }
 
-  if (confirmNumbers){
+  if (confirmNumbers) {
     characters = charsNumbers;
   }
-  
-  if (confirmSpecial){
+
+  if (confirmSpecial) {
     characters = charsSpecial;
   }
 
-  if (confirmUpper && confirmLower){
+  if (confirmUpper && confirmLower) {
     characters = charsLower.concat(charsUpper);
   }
 
-  if (confirmNumbers && confirmLower){
+  if (confirmNumbers && confirmLower) {
     characters = charsLower.concat(charsNumbers);
   }
 
-  if (confirmUpper && confirmNumbers){
+  if (confirmUpper && confirmNumbers) {
     characters = charsNumbers.concat(charsUpper);
   }
 
-  if (confirmSpecial && confirmLower){
+  if (confirmSpecial && confirmLower) {
     characters = charsLower.concat(charsSpecial);
   }
 
-  if (confirmUpper && confirmSpecial){
+  if (confirmUpper && confirmSpecial) {
     characters = charsSpecial.concat(charsUpper);
   }
 
-  if (confirmNumbers && confirmSpecial){
+  if (confirmNumbers && confirmSpecial) {
     characters = charsSpecial.concat(charsNumbers);
   }
 
-  if (confirmLower && confirmUpper && confirmNumbers){
+  if (confirmLower && confirmUpper && confirmNumbers) {
     characters = charsLower.concat(charsUpper, charsNumbers);
   }
 
-  if (confirmLower && confirmUpper && confirmSpecial){
+  if (confirmLower && confirmUpper && confirmSpecial) {
     characters = charsLower.concat(charsUpper, charsSpecial);
   }
 
-  if (confirmLower && confirmNumbers && confirmSpecial){
+  if (confirmLower && confirmNumbers && confirmSpecial) {
     characters = charsLower.concat(charsNumbers, charsSpecial);
   }
-  
-  if (confirmUpper && confirmNumbers && confirmSpecial){
+
+  if (confirmUpper && confirmNumbers && confirmSpecial) {
     characters = charsUpper.concat(charsNumbers, charsSpecial);
   }
 
-  if (confirmUpper && confirmNumbers && confirmSpecial && charsLower){
+  if (confirmUpper && confirmNumbers && confirmSpecial && charsLower) {
     characters = charsUpper.concat(charsNumbers, charsSpecial, charsLower);
     console.log(characters);
   }
+
+  let generatedpassword = [];
+
+  for (let i = 0; i < passwordLength; i++) {
+    let element = characters[Math.floor(Math.random() * characters.length)];
+    generatedpassword.push(element);
+  }
+  var displayedpasssword = generatedpassword.join("");
+  return displayedpasssword;
 }
